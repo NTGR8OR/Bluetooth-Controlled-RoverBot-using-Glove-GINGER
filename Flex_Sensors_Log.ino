7fd9f34348c351ac83135c0c72218083803f0773
Rakesh Raman <rakeshraman.r17@gmail.com>
2017-05-15 23:41:50 +0530

    Flex Sensors Log

---
+++ Flex_Sensors_Log.ino
@@ -0,0 +1,23 @@
+int F[4] = {A0, A1, A2, A3};
+int val = 0;
+
+void setup()
+{
+  Serial.begin(115200);
+  for (int i = 0; i < 4; i++)
+  {
+    pinMode(F[i], INPUT);
+  }
+}
+
+void loop()
+{
+  for (int i = 0; i < 4; i++)
+  {
+    val = analogRead(F[i]);
+    Serial.print(val);
+    Serial.print("  ");
+  }
+  Serial.println("");
+  delay(100);
+}
